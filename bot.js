bf9e4e1deb5073d933d17e3ca4b646a06890c8a9
Charlie Shaw <charlescshaw@gmail.com>
2021-03-28 14:19:58 -0500

    revert changes

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ function checkAPI() {
 			const channel = client.channels.cache.get(config.CHANNEL_ID);
 			// modify these lines to say whatever you want
 			channel.send('New Block Found!');
-			channel.send(JSON.stringify(body, [ "hash", "diff", "height", "value" ], 1));
+			channel.send(JSON.stringify(body[0]));
 
 			config.LAST_BLOCK_HASH = body[0].hash;
 			fs.writeFileSync('./config.json', JSON.stringify(config, null, '\t'));
