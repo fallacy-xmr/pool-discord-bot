a944e4feb3db599bfef86601f7dc8232887e3d80
Charlie Shaw <charlescshaw@gmail.com>
2021-04-05 11:50:25 -0500

    revert changes

--- bot.js
+++ bot.js
@@ -1,37 +1,28 @@
 const fs = require('fs');
 const request = require('request');
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require('./config.json');
 
 function checkAPI() {
 	request('https://'+config.API_DOMAIN+'/pool/blocks', { json: true }, (err, res, body) => {
 		if (err) { return console.log(err); }
-	
+
 		if (body[0].hash != config.LAST_BLOCK_HASH) {
 //			console.log('New Block Found: '+body[0].hash);  // uncoment to debug
 
 			const channel = client.channels.cache.get(config.CHANNEL_ID);
-			
-			embed = new Discord.MessageEmbed()
-				.setTitle('New Block Found!')
-				.setURL('https://xmrchain.net/block/'+newBlock.height)
-				.setColor(0xff6600) //make it monero orange
-//				.setDescription('Informative text to add at start')
-				.addField('Hash', newBlock.hash)
-				.addField('Diff', newBlock.diff)
-				.addField('Height', newBlock.height)
-				.addField('Value', newBlock.value);
-			
-			channel.send(embed);
+			// modify these lines to say whatever you want
+			channel.send('New Block Found!');
+			channel.send(JSON.stringify(body[0]));
 
 			config.LAST_BLOCK_HASH = body[0].hash;
 			fs.writeFileSync('./config.json', JSON.stringify(config, null, '\t'));
-			
+
 		}
-		
+
 	});
-	
+
 	setTimeout(checkAPI, 60000);
 }
 
