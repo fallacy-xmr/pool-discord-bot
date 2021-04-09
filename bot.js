4abac9e9bba25cddf914e94e9ea01b9878b91fc2
Charlie Shaw <charlescshaw@gmail.com>
2021-04-09 13:38:59 -0500

    revert to fallacy

--- bot.js
+++ bot.js
@@ -1,31 +1,27 @@
-console.log('Starting Bot..'); // uncomment to debug
-
 const fs = require('fs');
 const request = require('request');
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require('./config.json');
-
-console.log('Dependancies Loaded..'); // uncomment to debug
+const config = require("./config.json");
 
 function checkAPI() {
 	request('https://'+config.API_DOMAIN+'/pool/blocks', { json: true }, (err, res, body) => {
 		if (err) { return console.log(err); }
 	
 		if (body[0].hash != config.LAST_BLOCK_HASH) {
-			console.log('New Block Found: '+body[0].hash);  // uncoment to debug
+//			console.log('New Block Found: '+body[0].hash);  // uncoment to debug
 
 			const channel = client.channels.cache.get(config.CHANNEL_ID);
 			
 			embed = new Discord.MessageEmbed()
 				.setTitle('New Block Found!')
-				.setURL('https://xmrchain.net/block/'+body[0].height)
+				.setURL('https://xmrchain.net/block/'+newBlock.height)
 				.setColor(0xff6600) //make it monero orange
 //				.setDescription('Informative text to add at start')
-				.addField('Hash', body[0].hash)
-				.addField('Diff', body[0].diff)
-				.addField('Height', body[0].height)
-				.addField('Value', body[0].value);
+				.addField('Hash', newBlock.hash)
+				.addField('Diff', newBlock.diff)
+				.addField('Height', newBlock.height)
+				.addField('Value', newBlock.value);
 			
 			channel.send(embed);
 
@@ -36,16 +32,13 @@ function checkAPI() {
 		
 	});
 	
-	setTimeout(checkAPI, 120000);
+	setTimeout(checkAPI, 60000);
 }
 
-client.login(config.BOT_TOKEN);
-console.log ('Logged In..'); // uncomment to debug
 
-//client.on('debug', console.log); // uncomment to debug
+client.login(config.BOT_TOKEN);
 
 client.on('ready', () => {
-	console.log('Client Ready..'); // uncomment to debug
+//	console.log(`Logged in as ${client.user.tag}!`); // uncomment to debug
 	checkAPI();
-	console.log('Checking API..'); // uncomment to debug
 });
